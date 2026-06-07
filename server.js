581d11343f09b873018b44ada6852227c1abe597
roberto <robertoangelcarbone@gmail.com>
2026-06-06 23:28:10 -0400

    Usar process.env.PORT en server.js

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const path = require("path");
 
 // Crear aplicación
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
 
 // Activar CORS para permitir peticiones desde tu frontend
 app.use(cors());
